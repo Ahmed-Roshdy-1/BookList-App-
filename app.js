a30ac9a17287ca8102c53acae395ff215c41c4f1
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-04-16 04:00:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -78,6 +78,42 @@ class UI{
 
 // Store Class: Handles Storage
 
+class Srore{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books')===null){
+            books=[];
+        }else{
+            books=json.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static addBook(book){
+        const books = Srore.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+
+    static removeBook(book){
+        const books = Store.getBooks();
+
+        books.forEach((book,index) =>{
+            if(Book.isbn===isbn){
+                books.splice(index,1);
+            }
+        });
+        localStorage.setItem('books',JSON.books);
+
+    }
+
+   
+}
+
+
+
+
+
 // Event: Display Books
 
 document.addEventListener('DOMContentLoaded',UI.displayBooks);
